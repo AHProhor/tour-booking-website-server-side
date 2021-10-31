d89ba9a665ca1e356cc6e0d1d7bcdcb5f2ba9111
PROHOR <ahprohor88@gmail.com>
2021-11-01 02:11:54 +0600

    order added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,6 +20,7 @@ async function run(){
         await client.connect()
         const database = client.db('foding_travel');
         const tourCollection = database.collection('tours');
+        const orderCollection = database.collection('orders');
 
 
         // Get Tours API
@@ -28,12 +30,34 @@ async function run(){
             res.send(tours);
         });
 
-        // POST API
+        // Get Order Api
+        app.get('/orders', async(req,res) =>{
+            const cursor = orderCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
+        // Get Single Booking
+        app.get('/tours/:id',async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const tour = await tourCollection.findOne(query);
+            res.json(tour);
+        })
+
+        // POST API for add tour
         app.post('/tours', async (req, res) =>{
             const tour = req.body
             const result = await tourCollection.insertOne(tour);
             res.json(result);
         });
+        // Post API for add orders
+        app.post('/orders', async (req, res) =>{
+            const order = req.body
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        });
+        
     }
     finally{
         // await client.close();
